74ac3d9fc4bebe1fac61592d7ea8731cc7838a87
알파 <arpa1705@gmail.com>
2022-07-17 18:18:18 +0900

    Fix: 파일 이름 오타 수정

--- src/imgs/members/index.js
+++ src/imgs/members/index.js
@@ -1,5 +1,5 @@
 export { default as arpa } from "./arpa.png";
 export { default as coin } from "./coin.png";
 export { default as dacon } from "./dacon.png";
-export { default as gangjun } from "./gangjun06.gif";
-export { default as filename } from "./filename.gif";
+export { default as gangjun } from "./gangjun06.png";
+export { default as filename } from "./filename.png";
